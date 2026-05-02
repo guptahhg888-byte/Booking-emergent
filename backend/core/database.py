1785dc8cf8da438f48848b876e35bceb4fdc780f
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Air.local>
2026-05-02 17:32:20 +0530

    final changes added

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,6 +1,7 @@
 """MongoDB client singleton."""
+import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import MONGO_URL, DB_NAME
 
-mongo_client = AsyncIOMotorClient(MONGO_URL)
+mongo_client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 db = mongo_client[DB_NAME]
